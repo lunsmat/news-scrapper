6fbf8fc2f97e326900e07e4b4446b8f0d0f03796
Andrey Oliveira <dedey.cardoso@gmail.com>
2022-12-10 15:17:35 -0300

    Criação do Banco de dados do scrapper

---
+++ scrapper/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class  Noticia(models.Model):
+    id = models.IntegerField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    body = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
