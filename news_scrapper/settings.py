fbffefb963f2a22a6ccd3edb4c600baee24e878a
Andrey Oliveira <dedey.cardoso@gmail.com>
2022-12-13 23:02:45 -0300

    'commit'

--- news_scrapper/settings.py
+++ news_scrapper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scrapper'     # Novo app criado
+    'scrapper',     # Novo app criado
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'news_scrapper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'interface')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'interface')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'news_scrapper/interface')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- scrapper/models.py
+++ scrapper/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class  Noticia(models.Model):
-    id = models.IntegerField(max_length=100, primary_key=True)
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
     body = models.CharField(max_length=100)
